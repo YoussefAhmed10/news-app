8ae2238d028418a33ba17130713e83c5d1b5c977
youssef <ya9951638@gmail.com>
2021-07-25 18:46:06 +0200

    new theme

---
+++ lib/styles/theme.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+ThemeData lightTheme = ThemeData(
+  primarySwatch: Colors.deepOrange,
+  scaffoldBackgroundColor: Colors.white,
+  // app bar theme
+  appBarTheme: AppBarTheme(
+    titleSpacing: 20.0,
+    color: Colors.white,
+    elevation: 0.0,
+    backwardsCompatibility: false,
+    systemOverlayStyle: SystemUiOverlayStyle(
+      statusBarColor: Colors.white,
+      statusBarIconBrightness: Brightness.dark,
+    ),
+    titleTextStyle: TextStyle(
+      color: Colors.black,
+      fontSize: 22,
+      fontWeight: FontWeight.bold,
+    ),
+    iconTheme: IconThemeData(
+      color: Colors.black,
+      size: 30,
+    ),
+  ),
+  //FIB THEME
+  floatingActionButtonTheme: FloatingActionButtonThemeData(
+    backgroundColor: Colors.deepOrange,
+  ),
+  //BNB THEME
+  bottomNavigationBarTheme: BottomNavigationBarThemeData(
+    type: BottomNavigationBarType.fixed,
+    selectedItemColor: Colors.deepOrange,
+    elevation: 25.0,
+  ),
+  //TEXT THEME
+  textTheme: TextTheme(
+    bodyText1: TextStyle(
+      fontSize: 18.0,
+      fontWeight: FontWeight.w600,
+      color: Colors.black,
+    ),
+  ),
+);
+
+ThemeData darkTheme = ThemeData(
+  scaffoldBackgroundColor: HexColor('121212'),
+  //appbar theme dark
+  appBarTheme: AppBarTheme(
+    titleSpacing: 20.0,
+    color: HexColor('1f1f1f'),
+    elevation: 0.0,
+    backwardsCompatibility: false,
+    systemOverlayStyle: SystemUiOverlayStyle(
+      statusBarColor: HexColor('000000'),
+      statusBarIconBrightness: Brightness.light,
+    ),
+    titleTextStyle: TextStyle(
+      color: Colors.white,
+      fontSize: 22,
+      fontWeight: FontWeight.bold,
+    ),
+    iconTheme: IconThemeData(
+      color: Colors.white,
+      size: 30,
+    ),
+  ),
+  //fib dark theme
+  floatingActionButtonTheme: FloatingActionButtonThemeData(
+    backgroundColor: Colors.deepOrange,
+  ),
+  //bnb dark theme
+  bottomNavigationBarTheme: BottomNavigationBarThemeData(
+    type: BottomNavigationBarType.fixed,
+    backgroundColor: HexColor('1e1e1e'),
+    selectedItemColor: Colors.deepOrange,
+    unselectedItemColor: Colors.grey,
+    elevation: 25.0,
+  ),
+  textTheme: TextTheme(
+    bodyText1: TextStyle(
+      fontSize: 18.0,
+      fontWeight: FontWeight.w600,
+      color: Colors.white,
+    ),
+  ),
+  brightness: Brightness.dark,
+);
